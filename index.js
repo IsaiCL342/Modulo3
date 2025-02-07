64ba10b889d8b08b8ad2ef81f21b94d5cbf74136
IsaiCL <isaicampechanoleon@gmail.com>
2025-02-06 18:35:15 -0600

    Archivos iniciales

---
+++ index.js
@@ -0,0 +1,12 @@
+/*var nombre = prompt('Ingresa tu nombre')
+var edad = prompt('Ingresa tu edad')
+var gusto1 = prompt('Ingresa tu pasatiempo favorito')
+var gusto2 = prompt('Ingresa tu otro pasatiempo favorito')
+
+console.log(`Mi nombre es: ${nombre} tengo la edad de ${edad} me gusta ${gusto1} y tambien ${gusto2}`);*/
+
+var numero1 = Number (prompt('Ingresa el primer dato'))
+var numero2 = Number (prompt('Ingresa el segundo dato'))
+var numero3 = Number (prompt('Ingresa el tercer dato'))
+
+console.log(numero1+numero2+numero3);  
